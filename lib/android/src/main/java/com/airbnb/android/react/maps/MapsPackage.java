ba2271ee47b083518e20d70358eee397cc38e50a
Marija Rahman <marija.rahman@htecgroup.com>
2018-04-20 18:36:23 +0200

    override commented

--- lib/android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
@@ -24,7 +24,7 @@ public class MapsPackage implements ReactPackage {
     return Arrays.<NativeModule>asList(new AirMapModule(reactContext));
   }
 
-  @Override
+//  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
